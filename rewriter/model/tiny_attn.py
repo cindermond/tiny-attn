11b0b143e0c0d499035e911001a4d124b18951d5
=cindermond <=>
2022-08-27 22:41:10 -0500

    add gpt

--- rewriter/model/tiny_attn.py
+++ rewriter/model/tiny_attn.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from transformers.models.bart.modeling_bart import BartAttention
+from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
 
 
 class TinyAttention_(nn.Module):
@@ -66,5 +67,26 @@ class BartTinyAttention(nn.Module):
             new_hs, _, past_key_value = self.attention(new_hs, key_value_states=encoder_hidden_states, past_key_value = past_key_values, attention_mask=encoder_attention_mask)
         else:
             new_hs, _, past_key_value = self.attention(new_hs, past_key_value = past_key_values, attention_mask=attention_mask)
+        new_hs = self.linear2(new_hs)
+        return (new_hs, past_key_value)
+
+
+class GPT2TinyAttention(nn.Module):
+    def __init__(self, input_embd=1024, output_embd=1024, attention_embd=64, attention_head=1, attention_dropout=0.1) -> None:
+        super().__init__()
+        self.attention_embd = attention_embd
+        self.linear1 = nn.Linear(input_embd, attention_embd)
+        self.attention = BartAttention(attention_embd, attention_head, attention_dropout, is_decoder=True)
+        self.linear2 = nn.Linear(attention_embd, output_embd)
+        self.norm = nn.LayerNorm(input_embd)
+        with torch.no_grad():
+            for p in self.linear2.parameters():
+                p *= 0.01
+            
+
+    def forward(self, hidden_states, past_key_values = None, attention_mask = None):
+        new_hs = self.norm(hidden_states)
+        new_hs = self.linear1(new_hs)
+        new_hs, past_key_value = self.attention(new_hs, past_key_value = past_key_values, attention_mask=attention_mask)
         new_hs = self.linear2(new_hs)
         return (new_hs, past_key_value)
